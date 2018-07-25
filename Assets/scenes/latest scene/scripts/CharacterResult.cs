6a1845f9eda3833690b4564b3cbf9cc195ffd4b5
Unknown <britt-joosten@hotmail.com>
2018-07-25 11:34:24 +0200

    Character Names added to win screen

--- Assets/scenes/latest scene/scripts/CharacterResult.cs
+++ Assets/scenes/latest scene/scripts/CharacterResult.cs
@@ -13,8 +13,12 @@ public class CharacterResult : MonoBehaviour {
     public Sprite ch5;
     public Sprite ch6;
 
+    public Text highestIntName;
+    public Text lowestIntName;
+
     public GameObject spawnPointHighest;
     public GameObject spawnPointLowest;
+  
 
     //public Textvieuw tv;
     public Text charOmschrijving;
@@ -34,6 +38,7 @@ public class CharacterResult : MonoBehaviour {
             spawnPointLowest.GetComponent<Image>().enabled = true;
             spawnPointHighest.SetActive(true);
             resultChar = 1;
+            highestIntName.text = "De betrouwbare ";
             charOmschrijving.text = "Stick to the plan! Belofte maakt schuld. Kan en wil hard werken. Vindt geld verdienen belangrijk als basis van zekerheid.Komt beloftes en plichten na.Houdt zich aan wetten " +
                 "en regels en is niet geneigd daartegen te protesteren.Kan goed luisteren en geheimen bewaren. Is niet snel van mening te veranderen.Kan goed plannen en situaties overzien. Wil liever een vaste " +
                 "baan en een gewoon rijtjeshuis.Gebruikt geen drugs en drinkt weinig. Vindt dat mensen hun problemen zelf moeten oplossen.Heeft liefst een hond als huisdier. Is niet zo veel online, maar komt wel" +
@@ -47,6 +52,7 @@ public class CharacterResult : MonoBehaviour {
             spawnPointHighest.GetComponent<Image>().sprite = ch2;       
             spawnPointHighest.SetActive(true);
             resultChar = 2;
+            highestIntName.text = "Het Feestbeest ";
             charOmschrijving.text = " Yolo! Waar is het feestje..?! Levensgenieter, is graag het middelpunt. Altijd enthousiast. Zegt nooit nee.Ziet overal de leuke, positieve kanten.Durft risico’s te nemen.Denkt " +
                 "weinig na over gevaren en consequenties.Is impulsief. Zorgt dat hij / zij het zelf goed heeft. Gul met tijd en energie en sleept anderen mee.Wil dat het leuk blijft.Houdt van een feestje!Houdt niet " +
                 "van verplichtingen en afspraken.Is vluchtig in aandacht.Snel en oppervlakkig.Houdt niet van lezen en lang of diep nadenken.Houdt niet van wandelen in de natuur. Gaat graag shoppen en stappen." +
@@ -59,6 +65,7 @@ public class CharacterResult : MonoBehaviour {
             spawnPointHighest.GetComponent<Image>().sprite = ch3;
             spawnPointHighest.SetActive(true);
             resultChar = 3;
+            highestIntName.text = "De Idealist ";
             charOmschrijving.text = "Yes, we can!I have a dream!Wereldverbeteraar, probeert draagvlak te creëren om de wereld beter te krijgen. Gaat op de barricades als het moet, spreekt de politiek aan, demonstreert " +
                 "en tekent graag petities.Gelooft in verandering, houdt vast aan idealen en probeert anderen te overtuigen.Is bevlogen en heeft een positieve instelling.Heeft veel energie en is actief.Krijgt anderen " +
                 "enthousiast voor eigen ideeën. Woont in energie - neutrale woning, aan de rand van de stad. Leest de krant en is op de hoogte van het laatste nieuws.Ziet social media als plek om idealen te delen. Heeft " +
@@ -72,6 +79,7 @@ public class CharacterResult : MonoBehaviour {
             spawnPointLowest.GetComponent<Image>().enabled = true;
             spawnPointHighest.SetActive(true);
             resultChar = 4;
+            highestIntName.text = "De Loner ";
             charOmschrijving.text = "Waar is de nooduitgang?! Is het liefst alleen, trekt zich regelmatig terug uit de maatschappij. Maakt niet snel of makkelijk contact.Is onafhankelijk en trekt zich weinig aan van wat " +
                 "anderen vinden of zeggen.Past zich niet aan, aan de heersende norm, maar blijft zichzelf in verschillende situaties. Heeft een kleine groep hele goeie vrienden.Heeft een heel eigen gevoel voor humor.Houdt " +
                 "niet zo van feestjes en grote groepen mensen.Kan goed observeren.Geeft een mening als daarom gevraagd wordt.Kan goed voor zichzelf zorgen en wil later graag zelfvoorzienend leven zoals eigen stroom opwekken " +
@@ -86,6 +94,7 @@ public class CharacterResult : MonoBehaviour {
             spawnPointLowest.GetComponent<Image>().enabled = true;
             spawnPointHighest.SetActive(true);
             resultChar = 5;
+            highestIntName.text = "De Redder ";
             charOmschrijving.text = "Save the world! Is een goeie vriend die er graag voor anderen is. Kan goed luisteren en oplossend denken.Wordt aangetrokken door hulpzoekenden en voelt zich beter door anderen te helpen. " +
                 "Is bereid in te leveren of iets op te offeren, als een ander daar blij van wordt.Heeft altijd tijd, is nooit te druk en is flexibel.Is positief en ziet altijd wel mogelijkheden om dingen op te lossen. Kan anderen " +
                 "geruststellen en is rustig als iedereen in paniek raakt. Heeft de neiging om veel te geven en daardoor te weinig aan zichzelf te denken.Is goedgelovig en er wordt soms misbruik van hem / haar gemaakt. Heeft een sfeervol" +
@@ -99,6 +108,7 @@ public class CharacterResult : MonoBehaviour {
             spawnPointLowest.GetComponent<Image>().enabled = true;
             spawnPointHighest.SetActive(true);
             resultChar = 6;
+            highestIntName.text = "De rebel ";
             charOmschrijving.text = "Wil zijn eigen keuzes maken. Loopt niet blindelings achter de massa aan. Wil zich niet aanpassen en als dat betekend dat hij alleen staat, nou, dan is dat maar zo.Houd er niet van dat iemand de baas over hem is. " +
                 "Vindt vrijheid belangrijk zowel voor zichzelf als voor anderen. Heeft als ideaal een samenleving waarin niemand de leiding heeft en je met elkaar voor elkaar zorgt.Heeft oog voor mensen die het minder goed hebben en wil ze graag helpen. " +
                 "Woont in een kraakpand en heeft hond als huisdier.Gebruikt social media om zijn idealistische ideeën over de samenleving te delen en om op te roepen tot actie en protest als dat nodig is.Houdt van discussiëren en doet dit graag avonden lang. " +
@@ -121,6 +131,7 @@ public class CharacterResult : MonoBehaviour {
             spawnPointLowest.GetComponent<Image>().enabled = true;
             spawnPointLowest.SetActive(true);
             resultChar = 1;
+            lowestIntName.text = "De betrouwbare ";
         }
 
         if (allScores[1] == lowestInt)
@@ -130,6 +141,7 @@ public class CharacterResult : MonoBehaviour {
             spawnPointLowest.GetComponent<Image>().enabled = true;
             spawnPointLowest.SetActive(true);
             resultChar = 2;
+            lowestIntName.text = "Het Feestbeest ";
         }
 
         if (allScores[2] == lowestInt)
@@ -139,6 +151,7 @@ public class CharacterResult : MonoBehaviour {
             spawnPointLowest.GetComponent<Image>().enabled = true;
             spawnPointLowest.SetActive(true);
             resultChar = 3;
+            lowestIntName.text = "De Idealist ";
         }
 
         if (allScores[3] == lowestInt)
@@ -148,6 +161,7 @@ public class CharacterResult : MonoBehaviour {
             spawnPointLowest.GetComponent<Image>().enabled = true;
             spawnPointLowest.SetActive(true);
             resultChar = 4;
+            lowestIntName.text = "De Loner ";
         }
 
         if (allScores[4] == lowestInt)
@@ -157,6 +171,7 @@ public class CharacterResult : MonoBehaviour {
             spawnPointLowest.GetComponent<Image>().enabled = true;
             spawnPointLowest.SetActive(true);
             resultChar = 5;
+            lowestIntName.text = "De Redder ";
         }
 
         if (allScores[5] == lowestInt)
@@ -166,6 +181,7 @@ public class CharacterResult : MonoBehaviour {
             spawnPointLowest.GetComponent<Image>().enabled = true;
             spawnPointLowest.SetActive(true);
             resultChar = 6;
+            lowestIntName.text = "De rebel ";
         }
 
         return lowestInt;
